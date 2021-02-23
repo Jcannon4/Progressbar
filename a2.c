b446e5343f9d53504024d25d4ca8e9b24956a999
Jackson <jacksoncontreras4@gmail.com>
2021-02-23 03:09:24 -0800

    increased current

--- a2.c
+++ a2.c
@@ -15,7 +15,7 @@ typedef struct{
 void* progress_monitor (void * arg) {
    //struct prog_stat *threadProg = (struct prog_stat*) arg;
    prog_stat *threadProg = arg;
-   int i =0;
+   long i = 0;
     for(; i<threadProg->termination_value; i++)
     {
         usleep(250000);
@@ -25,30 +25,37 @@ void* progress_monitor (void * arg) {
     }
     return threadProg;
 }
-int wordcount (void * arg, char *filedesc) {
+int wordcount (prog_stat *arg, char *filedesc) {
     int iochar;
     
     //struct prog_stat *threadProg = (struct prog_stat*) arg;
     prog_stat *threadProg = arg;
+    
     FILE *fptr;
-    int i = 0;
+    long i = 0;
     printf("CALLING\n");
     char str[1];
     fptr = fopen(filedesc, "r");
+    long *init;
+    init = (long *)malloc(sizeof(threadProg->current_status));
+    //init = threadProg->current_status;
+    long current = threadProg->initial_value;
+    long term = threadProg->termination_value;
+    printf("current : %p\ninit : %ld\nBYTES: %ld\n", init, current, term);
     if(fptr != NULL){
         printf("file opened succesfully\n");
         for(; i< threadProg->termination_value; i++)
         {
             if(fgets(str, 1, fptr) != NULL)
             {
-                (threadProg->current_status)++;
-                printf("wordcount working : %p\n" , threadProg->current_status );
+                (*init)++;
+                printf("wordcount working : %ld\n" , *init );
             }
         
          } 
     }
     else{printf("ERROR OPENING FILE");}
-    return 2;
+    return 4;
     
 }
 
@@ -56,7 +63,7 @@ int main (int argc, char* argv[]){
     prog_stat *curProg = (prog_stat*)malloc(sizeof(prog_stat));
     //struct prog_stat curProg = PROGRESS_STATUS;
     struct stat buff;
-    int initial_value = 0;
+    //int initial_value = 0;
     
     char *fd = argv[1];
     printf("arg array: %s\t%s\n", argv[0], argv[1]);
@@ -74,12 +81,15 @@ int main (int argc, char* argv[]){
         exit(2);
     }
     pthread_t newthread;
+    pthread_attr_t	pthread_attributes;
     int *result;
-    pthread_create(&newthread, NULL, progress_monitor, &curProg);
-    int iochar = wordcount(&curProg, argv[1]);
+    printf("BYTE SIZE CHECK : %ld\n", curProg->termination_value);
+    pthread_attr_init(&pthread_attributes);
+    pthread_create(&newthread, &pthread_attributes, progress_monitor, &curProg);
+    wordcount(curProg, argv[1]);
     pthread_join(newthread, (void *)&result);
     printf("\ncalling before seg fault\n");
-    printf("THREAD DONE result=%d\nwordcount = %d\n", *result, iochar);
+    printf("THREAD DONE result=%d\nwordcount = \n", *result);
     return 0;
     
 }
